42de2910eb655d6ffc5f4dc7805915bd28f2f336
Ph-uad <temidayo26j@gmail.com>
2023-07-15 23:30:55 +0100

    added Dom data object

---
+++ src/app/json/domData.js
@@ -0,0 +1,21 @@
+export const DomData = {
+   bio : {
+        id: "01",
+        title: "bio",
+        message: [
+            `I am a web developer, on the path to hone the skill of web development by 
+             testing relative concepts used by the industry to build web interfacesthat deliver clear 
+             and intended message with appealing designs.`,
+            ` I am focused on web performance and design concepts. I love learning new concepts that brings
+             intended results to web performances.`
+            ]
+    },
+    insight : {
+        id: "02",
+        title: "insight",
+        message : [
+            `Fiction to me is a source of inspiration, like thoughts and dreams, fictional works seems to let 
+             us see someone elses thoughts or dreams in the same way. `
+        ]
+    }
+}
