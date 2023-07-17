26779828b4df90ff94dd3008043a61e4ae33afce
Ph-uad <temidayo26j@gmail.com>
2023-07-17 08:44:55 +0100

    changed font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,24 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({ weight: ['400', '300'], subsets: ['latin'] })
 
 export const metadata = {
   title: {
-    default : 'Phuad',
-    template : '%s | Phuad'
+    default: 'Phuad',
+    template: '%s | Phuad'
   },
   description: 'Discover the works of a web developer, with the focus of delivering efficient and well designed website.',
-  keywords : ['My Portfolio', 'Next.js'],
+  keywords: ['My Portfolio', 'Next.js'],
 
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-        <link rel="icon" href="./favicon.ico" sizes="any" />
+      <link rel="icon" href="./favicon.ico" sizes="any" />
 
-      <body className={ inter.className }>{ children }</body>
+      <body className={ roboto.className }>{ children }</body>
     </html>
   )
 }
