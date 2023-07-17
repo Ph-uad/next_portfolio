1d76baf5b03f0753baa21b8f043903aff47fd074
Ph-uad <temidayo26j@gmail.com>
2023-07-17 10:01:34 +0100

    more blog data

--- src/app/json/Blogs.js
+++ src/app/json/Blogs.js
@@ -1,25 +1,25 @@
-import { Charity, DigitalTransformation, Roles } from "@/public";
+import { Bridge, Charity, Committee, DigitalTransformation, Roles } from "@/public";
 
 const blogs = [
-    {
-        title: "Digital Transformation",
-        poster: DigitalTransformation.src,
-        link: "https://medium.com/@jgtkc/digital-transformation-9da4e33f71fa"
-    },
     {
         title: "Roles Essential to Digital Transformation",
         poster: Roles.src,
         link: "https://medium.com/@jgtkc/roles-essential-to-digital-transformation-2718c03e5cee"
     },
     {
-        title: "Monochrome",
-        poster: Charity.src,
-        link: "https://ph-uad.github.io/figma-Foundation/"
+        title: "Digital Transformation",
+        poster: DigitalTransformation.src,
+        link: "https://medium.com/@jgtkc/digital-transformation-9da4e33f71fa"
     },
     {
-        title: "Monochrome",
-        poster: Charity.src,
-        link: "https://ph-uad.github.io/figma-Foundation/"
+        title: "Business liaison",
+        poster: Bridge.src,
+        link: "https://medium.com/@jgtkc/business-and-it-liaisons-5a784be8a96e"
+    },
+    {
+        title: "Steering Committee",
+        poster: Committee.src,
+        link: "https://medium.com/@jgtkc/the-steering-committee-3591b745008c"
     },
 ]
 
